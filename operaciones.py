e32ccdf72e5100fa47a66ac05b81f4964852f089
saray <saraydcardenasr15@gmail.com>
2025-03-27 13:08:26 -0500

    añadi división

--- operaciones.py
+++ operaciones.py
@@ -1,1 +1,9 @@
-# en este van opderaciones 
+import math
+def division (a,b):
+operacion=math.log(a)
+operacion=math.log(b)
+operacion=(a,b)
+return operacion
+a=int(input('ingrese primer valor:'))
+b=int(input('ingrese segundo valor:'))
+print('la division es:'division(a,b))
