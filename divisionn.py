b06bc365a53118ddb9d46087d14d5a59c69484bb
Mariana <marianapirachican94@gmail.com>
2025-03-27 13:13:35 -0500

    Se agrego la funcion division entera a la rama2

---
+++ divisionn.py
@@ -0,0 +1,6 @@
+def funcion(a,b):
+    operacion2=(a//b)
+    return operacion2
+a=int(input('Ingresa el dividendo'))
+b=int(input('Ingresa el divisor'))
+division=print('El cociente de la division es:',funcion(a,b))
